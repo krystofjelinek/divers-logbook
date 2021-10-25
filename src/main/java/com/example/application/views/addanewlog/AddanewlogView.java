53f972e8bad0757f704ff518eb9c1bb6407d9109
start.vaadin.com <no-reply@vaadin.com>
2021-10-25 13:04:51 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/addanewlog/AddanewlogView.java
@@ -0,0 +1,31 @@
+package com.example.application.views.addanewlog;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.example.application.views.MainLayout;
+
+@PageTitle("Add a new log")
+@Route(value = "empty1", layout = MainLayout.class)
+public class AddanewlogView extends VerticalLayout {
+
+    public AddanewlogView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
